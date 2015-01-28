0cdcc751bdc1434bb1472acc42617ed8f29742a2
user <user@user.com>
2015-01-27 22:05:23 -0600

    get rid of now-unnecessary  BoggleTest class (keep the object)

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -12,3 +12,34 @@ class Board(val letters: Array[Array[Char]])
   def get(i: Int, j: Int) = letters(i)(j)
   def has(i: Int, j: Int) = (i >= 0) && (i < _height) && (j >= 0) && (j < _width)
 }
+
+object Board
+{
+  def makeBoard(boardLines: Seq[String]) =
+  {
+    //detect board size, then create a board
+    val boardHeight = boardLines.length
+    if (boardHeight == 0) throw new Exception("no lines found in board file")
+    val lineLength = boardLines(0).length
+
+    {
+      //check that all line lengths are equal
+      val otherLengthLines = boardLines.filter(_.length != lineLength)
+      if (otherLengthLines.length != 0) throw new Exception("non-rectangular board detected")
+    }
+
+    val boardWidth = (lineLength + 1) / 2 //each character except for last is followed by a single space
+    val boardArray: Array[Array[Char]] = Array.ofDim[Char](boardHeight, boardWidth)
+    var lineNumber = 0
+    for (line <- boardLines) {
+      var charCounter = 0
+      for (i <- 0 until boardWidth) {
+        boardArray(lineNumber)(i) = line(charCounter)
+        charCounter += 2
+      }
+      lineNumber += 1
+    }
+
+    new Board(boardArray)
+  }
+}
--- src/main/scala/BoggleTest.scala
+++ src/main/scala/BoggleTest.scala
@@ -19,10 +19,10 @@ object BoggleTest extends App
       try
       {
         val dictSource = Source.fromFile(args(0))
-
+        //lowercase everything for simplicity and durability
         val boardLines = boardSource.getLines().toList.map(_.toLowerCase)
         val dictLines = dictSource.getLines().toList.map(_.toLowerCase)
-        val resultList = new BoggleTest(dictLines, boardLines).run
+        val resultList = new BoggleSolver(Board.makeBoard(boardLines), dictLines).solve
 
         //write results
         val outputFileWriter = new PrintWriter(new File(args(2)))
@@ -32,42 +32,5 @@ object BoggleTest extends App
       catch {case e: java.io.FileNotFoundException => println("ERROR: Cannot open dict file.")}
     }
     catch {case e: java.io.FileNotFoundException => println("ERROR: Cannot open board file.")}
-  }
-}
-
-class BoggleTest(val words: Seq[String], val boardLines: Seq[String])
-{
-  def run =
-  {
-    //detect board size, then create a board
-    val boardHeight = boardLines.length
-    if (boardHeight == 0) throw new Exception("no lines found in board file")
-    val lineLength = boardLines(0).length
-
-    {
-      //check that all line lengths are equal
-      val otherLengthLines = boardLines.filter(_.length != lineLength)
-      if (otherLengthLines.length != 0) throw new Exception("non-rectangular board detected")
-    }
-
-    val boardWidth = (lineLength + 1) / 2 //each character except for last is followed by a single space
-    val boardArray = Array.ofDim[Char](boardHeight, boardWidth)
-    var lineNumber = 0
-    for (line <- boardLines)
-    {
-      var charCounter = 0
-      for (i <- 0 until boardWidth)
-      {
-        boardArray(lineNumber)(i) = line(charCounter)
-        charCounter += 2
-      }
-      lineNumber += 1
-    }
-    val board = new Board(boardArray)
-
-    //test each word against board
-    //lowercase all words, just to be safe
-    //encapsulate solve in a class instance to ease transition to parallelized solvers
-    new BoggleSolver(board, words).solve
   }
 }
