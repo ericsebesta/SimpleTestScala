44b6e0c94ce8a4fb80ca33c0ccf525195a830120
user <user@user.com>
2015-01-31 00:01:42 -0600

    convert dictionary file reading to use Iterators, which allow lazy evaluation (and therefore save a lot of memory from loading the whole file into memory)
    Also make all tests sequential so that we can add timing in the future

--- src/main/scala/BoggleSolver.scala
+++ src/main/scala/BoggleSolver.scala
@@ -5,16 +5,16 @@ package esebesta.BoggleSolverScala
  * Also note that BoggleSolver does not modify any input values, making it purely functional
  *
  * @param board a boggle board
- * @param dict a sequence of lowercase words
+ * @param dict a sequence of lowercase words, passed as a an iterator to allow lazy evaluation (load) of large data sets
  * @param minWordLength the minimum length a word in the dict list must be to be considered a valid word
  */
-class BoggleSolver(val board: Board, val dict: Seq[String], val minWordLength: Int = 3)
+class BoggleSolver(val board: Board, val dict: Iterator[String], val minWordLength: Int = 3)
 {
   /**
    * Find the words that are on the Boggle board, given the rule set
    * @return the found words
    */
-  def solve = dict.filter(isOnBoard)
+  def solve = dict.filter(isOnBoard).toList
 
   private def isOnBoard(word: String): Boolean = {
     //if word < 3 letters, not valid
--- src/test/scala/TestBoggleSolver.scala
+++ src/test/scala/TestBoggleSolver.scala
@@ -5,11 +5,11 @@ import scala.io.Source
 
 class TestBoggleSolver extends Specification
 {
-	val dictionaryList = Source.fromFile("data/dictionary.txt").getLines().toList
-	val dictionaryLargeList = Source.fromFile("data/dictionary_large.txt").getLines().toList
 	val board1 = Board.makeBoard(Source.fromFile("data/board_1.txt").getLines().toList.map(_.toLowerCase))
 	val board2 = Board.makeBoard(Source.fromFile("data/board_2.txt").getLines().toList.map(_.toLowerCase))
 	val board6 = Board.makeBoard(Source.fromFile("data/board_6_pvt.txt").getLines().toList.map(_.toLowerCase))
+
+	sequential
 
 	"solve" should
 	{
@@ -17,12 +17,13 @@ class TestBoggleSolver extends Specification
 		{
 			val boardLines = List(("p w y r"), ("e n t h"), ("g s i q"), ("o l s a"))
 			val dictLines = List("foozle", "quit")
-			val resultList = new BoggleSolver(Board.makeBoard(boardLines), dictLines).solve
+			val resultList = new BoggleSolver(Board.makeBoard(boardLines), dictLines.iterator).solve
 			resultList mustEqual List("quit")
 		}
 
 		"solve board_1 with a regular dictionary" in
 		{
+			val dictionaryList = Source.fromFile("data/dictionary.txt").getLines()
 			solve(board1,
 				dictionaryList,
 				"data/board_1_solution.txt")
@@ -30,6 +31,7 @@ class TestBoggleSolver extends Specification
 
 		"solve board_2 with a regular dictionary" in
 		{
+			val dictionaryList = Source.fromFile("data/dictionary.txt").getLines()
 			solve(board2,
 				dictionaryList,
 				"data/board_2_solution.txt")
@@ -52,6 +54,7 @@ class TestBoggleSolver extends Specification
 
 		"solve board_6 with a regular dictionary" in
 		{
+			val dictionaryList = Source.fromFile("data/dictionary.txt").getLines()
 			solve(board6,
 				dictionaryList,
 				"data/board_6_solution.txt")
@@ -59,6 +62,7 @@ class TestBoggleSolver extends Specification
 
 		"solve board_1 with a large dictionary" in
 			{
+				val dictionaryLargeList = Source.fromFile("data/dictionary_large.txt").getLines()
 				solve(board1,
 					dictionaryLargeList,
 					"data/board_1_solution_large.txt")
@@ -66,6 +70,7 @@ class TestBoggleSolver extends Specification
 
 		"solve board_2 with a large dictionary" in
 		{
+			val dictionaryLargeList = Source.fromFile("data/dictionary_large.txt").getLines()
 			solve(board2,
 				dictionaryLargeList,
 				"data/board_2_solution_large.txt")
@@ -73,13 +78,14 @@ class TestBoggleSolver extends Specification
 
 		"solve board_6 with a large dictionary" in
 		{
+			val dictionaryLargeList = Source.fromFile("data/dictionary_large.txt").getLines()
 			solve(board6,
 				dictionaryLargeList,
 				"data/board_6_solution_large.txt")
 		}
 	}
 
-	def solve(board: Board, dict: Seq[String], solutionFile: String) =
+	def solve(board: Board, dict: Iterator[String], solutionFile: String) =
 	{
 		//lowercase everything for simplicity and durability
 		val resultList = new BoggleSolver(board, dict).solve
